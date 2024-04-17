ca316dc4a5487b647d379bf9560b2a0d45b1e44b
Syeed Mohd Saquib <101272076+syeedsaquib@users.noreply.github.com>
2024-04-17 09:53:29 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,288 @@
+let btn = document.querySelector("button");
+
+let inpt = document.querySelector("input");
+let bod = document.querySelector("body");
+
+inpt.addEventListener("change", function(){
+  bod.style.backgroundColor = this.value;
+})
+
+
+
+//  btn.onclick = function(){
+//     for(let i=1;i>0;i++){
+//       console.log(i);
+//       console.log("SYstem hang !!");
+//       for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }
+//     }
+//     for(let i=1;i>0;i++){
+//       console.log(i);
+//       console.log("SYstem hang !!");
+//     }for(let i=1;i>0;i++){
+//       console.log(i);
+//       console.log("SYstem hang !!");
+//     }for(let i=1;i>0;i++){
+//       console.log(i);
+//       console.log("SYstem hang !!");
+//     }for(let i=1;i>0;i++){
+//       console.log(i);
+//       console.log("SYstem hang !!");
+//     }for(let i=1;i>0;i++){
+//       console.log(i);
+//       console.log("SYstem hang !!");
+//     }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }for(let i=1;i>0;i++){
+//          console.log(i);
+//          console.log("SYstem hang !!");
+//        }
+//  }
+ 
+
+
+
+
